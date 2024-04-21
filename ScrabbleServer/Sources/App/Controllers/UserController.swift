c6839653efe9f9450d5be8585d77a498a5a2173a
3ybacTuK <3ybactuk1337@gmail.com>
2024-04-21 14:26:53 +0300

    add controllers, app working, postgres not connecting

--- ScrabbleServer/Sources/App/Controllers/UserController.swift
+++ ScrabbleServer/Sources/App/Controllers/UserController.swift
@@ -3,32 +3,32 @@ import Vapor
 
 struct UserController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
-//        let todos = routes.grouped("todos")
-//
-//        todos.get(use: { try await self.index(req: $0) })
-//        todos.post(use: { try await self.create(req: $0) })
-//        todos.group(":todoID") { todo in
-//            todo.delete(use: { try await self.delete(req: $0) })
-//        }
+        let users = routes.grouped("users")
+
+        users.get(use: { try await self.index(req: $0) })
+        users.post(use: { try await self.create(req: $0) })
+        users.group(":userID") { user in
+            user.delete(use: { try await self.delete(req: $0) })
+        }
     }
-//    
-//    func index(req: Request) async throws -> [Todo] {
-//        try await Todo.query(on: req.db).all()
-//    }
-//
-//    func create(req: Request) async throws -> Todo {
-//        let todo = try req.content.decode(Todo.self)
-//
-//        try await todo.save(on: req.db)
-//        return todo
-//    }
-//
-//    func delete(req: Request) async throws -> HTTPStatus {
-//        guard let todo = try await Todo.find(req.parameters.get("todoID"), on: req.db) else {
-//            throw Abort(.notFound)
-//        }
-//
-//        try await todo.delete(on: req.db)
-//        return .noContent
-//    }
+
+    func index(req: Request) async throws -> [User] {
+        try await User.query(on: req.db).all()
+    }
+
+    func create(req: Request) async throws -> User {
+        let user = try req.content.decode(User.self)
+
+        try await user.save(on: req.db)
+        return user
+    }
+
+    func delete(req: Request) async throws -> HTTPStatus {
+        guard let user = try await User.find(req.parameters.get("userID"), on: req.db) else {
+            throw Abort(.notFound)
+        }
+
+        try await user.delete(on: req.db)
+        return .noContent
+    }
 }
--- ScrabbleServer/Sources/App/configure.swift
+++ ScrabbleServer/Sources/App/configure.swift
@@ -12,16 +12,17 @@ public func configure(_ app: Application) async throws {
     app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+        username: Environment.get("DATABASE_USERNAME") ?? "username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "password",
+        database: Environment.get("DATABASE_NAME") ?? "scrabbledb",
         tls: .prefer(try .init(configuration: .clientDefault)))
     ), as: .psql)
 
-    app.migrations.add(CreateTodo())
+    app.migrations.add(CreatePlayer())
+    app.migrations.add(CreateRoom())
+    app.migrations.add(CreateUser())
 
     app.views.use(.leaf)
-
 
     // register routes
     try routes(app)
